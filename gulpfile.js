cbe186207b8e077343e1f66071f005d9d0b1c435
Matthew Caldwell <caldwellysr@gmail.com>
2016-11-07 06:41:26 -0500

    add gulp templating engine for reuseable partials

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,39 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var template = require('gulp-template-html');
 
 gulp.task('js', function() {
     return gulp.src(['./js/*.js', './js/**/*.js'])
             .pipe(concat('app.js'))
             .pipe(uglify())
-            .pipe(gulp.dest('.'))
+            .pipe(gulp.dest('./dist/js/'))
             .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function() {
     return gulp.src("./sass/*.scss")
             .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
-            .pipe(gulp.dest("./css"))
+            .pipe(gulp.dest("./dist/css"))
             .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['js', 'sass'], function() {
+gulp.task('html', function() {
+    return gulp.src('./src/content/*.html')
+            .pipe(template('./src/templates/template.html'))
+            .pipe(gulp.dest('dist'))
+            .pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['js', 'sass', 'html'], function() {
 
     browserSync.init({
-        server: "./"
+        server: "./dist/"
     });
 
-    gulp.watch('js/**/*.js', ['js']);
-    gulp.watch('sass/**/*.scss', ['sass']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/templates/**/*.html', ['html']);
+    gulp.watch('src/content/**/*.html', ['html']);
+    gulp.watch('./dist/*.html').on('change', browserSync.reload);
 });
